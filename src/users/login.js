7b070d3334eef971e63b62f2ceb15801375cd4d1
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-04-25 01:01:45 +0200

    add password validation

--- src/users/login.js
+++ src/users/login.js
@@ -2,19 +2,23 @@ import { app } from "../app";
 import { check, validationResult } from "express-validator";
 import { getToken } from "./login-service";
 
-app.post("/login", [check("username").exists()], async (req, resp) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return resp.status(422).json({ errors: errors.array() });
+app.post(
+  "/login",
+  [check("username").exists(), check("password").exists()],
+  async (req, resp) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return resp.status(422).json({ errors: errors.array() });
+    }
+    try {
+      const username = req.body.username;
+      const password = req.body.password;
+      const user = { name: username, password: password };
+      const token = await getToken(user);
+      if (token) resp.json({ accessToken: token });
+      else resp.status(500).send();
+    } catch (e) {
+      resp.status(500).send();
+    }
   }
-  try {
-    const username = req.body.username;
-    const password = req.body.password;
-    const user = { name: username, password: password };
-    const token = await getToken(user);
-    if (token) resp.json({ accessToken: token });
-    else resp.status(500).send();
-  } catch (e) {
-    resp.status(500).send();
-  }
-});
+);
